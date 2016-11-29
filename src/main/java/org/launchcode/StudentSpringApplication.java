2082979be96d7a6a89873ca91567393681c06cd7
abarla <haritha.manchili@gmail.com>
2016-11-29 13:35:33 -0600

    commit

---
+++ src/main/java/org/launchcode/StudentSpringApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentSpringApplication.class, args);
+	}
+}
